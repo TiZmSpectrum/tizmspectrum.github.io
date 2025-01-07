ab05bb3dfc576362d4463d8c78c35c0b40884a4b
Karl Fryzek <117841207+TiZmSpectrum@users.noreply.github.com>
2025-01-06 20:24:22 -0500

    the protoweb update
    now here

--- fakead.php
+++ fakead.php
@@ -1,11 +1,11 @@
 <?php
-$imagesDir = 'fakeads/horizontal/';
-$array = array(
-    "f" => "bar",
-    "bar" => "foo",
-);
 
-$images = glob($imagesDir . '*.{png}', GLOB_BRACE);
+$database = new SQLite3('ads.db');
 
-$randomhorImage = $images[array_rand($images)]; // See comments
+$ads_query = $database->query('SELECT * FROM ads ORDER BY RANDOM() LIMIT 1');	
+$ad = $ads_query->fetchArray(SQLITE3_ASSOC);
+	
+$ad_image = 'fakeads/horizontal/' . $ad['image'];
+$ad_url = $ad['url'];
+
 ?>
